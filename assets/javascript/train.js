75995584ae2b83aec06777f89bce5fe1b51c4c49
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-03-17 15:45:00 -0400

    updated files

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDwUvroRMn-GhNWQ25LilaYRXJzcQ-ht-M",
+    authDomain: "trainactivity-8a22f.firebaseapp.com",
+    databaseURL: "https://trainactivity-8a22f.firebaseio.com",
+    projectId: "trainactivity-8a22f",
+    storageBucket: "trainactivity-8a22f.appspot.com",
+    messagingSenderId: "48539152408"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
